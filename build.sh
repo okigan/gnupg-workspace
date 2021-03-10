dd25db4a6680c56deea748c24426f216a01232ac
Igor Okulist <okigan@gmail.com>
2021-03-10 00:17:03 -0800

    Add github workflow

--- build.sh
+++ build.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-pushd gnupg
-./autogen.sh
-popd
-
-pushd build-gnupg
-CFLAGS="-ggdb3 -O0 -DDEBUG -fsanitize=address  -fno-omit-frame-pointer" ../gnupg/configure --enable-maintainer-mode
-make
-popd
+#!/bin/env bash
